1101d68f66f6613262bcac4a85062cf2b66bc72f
marrrrryyy <maria.daniluk96@gmail.com>
2021-01-20 18:34:41 +0100

    rocket launched

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("hello friend");
+var doCoolStuff = function() {
+    var currentClassName = document.getElementById("cool").className;
+    if(currentClassName == "cool") {
+        document.getElementById("cool").className = "cool red";
+    } else {
+    document.getElementById("cool").className = "cool";
+    }
+};
