61b8f06e8f28d9ebe827ad3b195f2bc115aecd4d
marrrrryyy <maria.daniluk96@gmail.com>
2021-01-20 10:11:42 +0100

    some testing

---
+++ test.js
@@ -0,0 +1,29 @@
+var favColor = "blue";
+var myFavColors = ["blue","red", "violet"];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+    firstName: "Mary",
+    lastName: "D",
+    favColors: ["blue", "red", "white"],
+    yearsAlive: 100,
+    isMale: false
+};
+var whatIsMyFavColor = function(){
+    return "violet";
+}
+/*
+function whatIsMyFavColor () {
+
+}*/
+
+var doSomething = function() {
+    console.log("do something!");
+}
+
+if (true) {
+    alert("male");
+};
+if ("male" == "male") {
+    alert("male");
+};
